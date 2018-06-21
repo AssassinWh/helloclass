3197e79768424f3f00fb7fc05477d77cce6dc979
AssassinWh <1421273218@qq.com>
2018-06-21 16:05:35 +0800

    my second code

---
+++ helloclass.cpp
@@ -0,0 +1,17 @@
+#include "helloclass.h"
+
+HelloClass::HelloClass()
+{
+   cout<<" I'm being created!"<<endl;
+}
+
+HelloClass::~HelloClass()
+{
+   cout<<" I'm being destroyed!"<<endl;
+}
+
+void HelloClass::sayHello()
+{
+   cout<<" Hello!"<<endl;
+}
+
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "helloclass.h"
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+  HelloClass A;
+  A.sayHello();
+  return 0;
+}
